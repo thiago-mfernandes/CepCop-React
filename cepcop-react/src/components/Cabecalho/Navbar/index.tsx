e3c0783e9589e4208fca1ac436c693c7f20c9ce8
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-04-22 16:40:02 -0300

    refatoracao de arquivo de styled components da Navbar

--- cepcop-react/src/components/Cabecalho/Navbar/index.tsx
+++ cepcop-react/src/components/Cabecalho/Navbar/index.tsx
@@ -1,142 +1,42 @@
 import {
-  azulEscuroFosco,
-  cinzaEscuro,
-  cinzaMedio,
-} from "src/components/UI/variaveis";
-import styled from "styled-components";
-import {
-  MdDescription,
-  MdGroups,
-  MdHome,
-  MdOutlineSettings,
-  MdPendingActions,
-  MdPeopleOutline,
-  MdSchool,
-  MdSupervisorAccount,
+    MdDescription,
+    MdGroups,
+    MdHome,
+    MdOutlineSettings,
+    MdPendingActions,
+    MdPeopleOutline,
+    MdSchool,
+    MdSupervisorAccount,
 } from "react-icons/md";
 import { FaGraduationCap } from "react-icons/fa";
 
-const NavLista = styled.ul`
-  display: flex;
-  flex-direction: column;
-  //-------------------------------------------->
-  @media screen and (max-width: 768px) {
-    border: none;
-    display: flex;
-    flex-direction: row;
-    height: 25vh;
-    justify-content: flex-end;
-    overflow-x: auto;
-    width: 100%;
-  }
-`;
-
-const NavItem = styled.li`
-  align-items: center;
-  border-bottom: 1px solid ${cinzaMedio};
-  display: flex;
-  flex-direction: row;
-  height: 10vh;
-  justify-content: flex-start;
-  text-decoration: none;
-  width: 100%;
-
-  &:first-child {
-    border-top: 1px solid ${cinzaMedio};
-    margin-left: 0;
-    margin-top: 7.5%;
-    //-------------------------------------------->
-    @media screen and (max-width: 768px) {
-      border-bottom: 1px solid ${cinzaMedio}; //pode retirar se quiser
-      border-top: none;
-      flex-direction: row-reverse;
-      justify-content: space-between;
-      margin-top: 0;
-      position: absolute;
-      left: 0;
-      top: 5%;
-      width: 100vw;
-    }
-  }
-
-  &:hover {
-    border-left: 4px solid ${azulEscuroFosco};
-    cursor: pointer;
-    //adicionar o material icons aqui
-    //.material-icons {
-    /* color: $azul-medio-brilhante;
-                }
-                a {
-                    color: black;
-                } */
-
-    //-------------------------------------------->
-    @media screen and (max-width: 768px) {
-      border-left: none;
-    }
-  }
-  //-------------------------------------------->
-  @media screen and (max-width: 768px) {
-    align-items: center;
-    border-bottom: none;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    margin: 0 5%;
-    width: 100%;
-  }
-`;
-
-const NavIcon = styled.i`
-  color: ${cinzaEscuro};
-  cursor: pointer;
-  font-size: 0.8rem;
-  padding: 0 7.5%;
-
-  //---------------------------------------------------->
-  @media screen and (max-width: 768px) {
-    padding: 0;
-  }
-`;
-
-const NavLink = styled.p`
-  color: ${cinzaEscuro};
-  font-size: 1rem;
-  font-family: "Lato", sans-serif;
-  text-decoration: none;
-  width: 100%;
-  //-------------------------------------------->
-  @media screen and (max-width: 768px) {
-    width: 75%;
-    padding-left: 10%;
-  }
-`;
+import { NavLista, NavItem, NavIcon, NavLink } from "./styledComponents";
 
 function Navbar() {
-  const navItens = [
-    { id: 1, icone: <MdHome />, nome: "Home" },
-    { id: 2, icone: <MdSupervisorAccount />, nome: "Coordenadores" },
-    { id: 3, icone: <MdPeopleOutline />, nome: "Supervisores" },
-    { id: 4, icone: <MdSchool />, nome: "Estagiários " },
-    { id: 5, icone: <MdDescription />, nome: "Analisantes" },
-    { id: 6, icone: <MdPendingActions />, nome: "Atendimentos" },
-    { id: 7, icone: <MdGroups />, nome: "Turmas" },
-    { id: 8, icone: <FaGraduationCap />, nome: "Cursos" },
-    { id: 9, icone: <MdOutlineSettings />, nome: "Configurações" },
-  ];
+    const navItens = [
+        { id: 1, icone: <MdHome />, nome: "Home" },
+        { id: 2, icone: <MdSupervisorAccount />, nome: "Coordenadores" },
+        { id: 3, icone: <MdPeopleOutline />, nome: "Supervisores" },
+        { id: 4, icone: <MdSchool />, nome: "Estagiários " },
+        { id: 5, icone: <MdDescription />, nome: "Analisantes" },
+        { id: 6, icone: <MdPendingActions />, nome: "Atendimentos" },
+        { id: 7, icone: <MdGroups />, nome: "Turmas" },
+        { id: 8, icone: <FaGraduationCap />, nome: "Cursos" },
+        { id: 9, icone: <MdOutlineSettings />, nome: "Configurações" },
+    ];
 
-  return (
-    <nav>
-      <NavLista>
-        {navItens.map((navitem, key) => (
-          <NavItem key={key}>
-            <NavIcon>{navitem.icone}</NavIcon>
-            <NavLink>{navitem.nome}</NavLink>
-          </NavItem>
-        ))}
-      </NavLista>
-    </nav>
-  );
+    return (
+        <nav>
+            <NavLista>
+                {navItens.map((navitem, key) => (
+                    <NavItem key={key}>
+                        <NavIcon>{navitem.icone}</NavIcon>
+                        <NavLink>{navitem.nome}</NavLink>
+                    </NavItem>
+                ))}
+            </NavLista>
+        </nav>
+    );
 }
 
 export default Navbar;
---
+++ cepcop-react/src/components/Cabecalho/Navbar/styledComponents.ts
@@ -0,0 +1,102 @@
+import {
+    azulEscuroFosco,
+    cinzaEscuro,
+    cinzaMedio,
+  } from "src/components/UI/variaveis";
+import styled from "styled-components";
+  
+export const NavLista = styled.ul`
+    display: flex;
+    flex-direction: column;
+    //-------------------------------------------->
+    @media screen and (max-width: 768px) {
+      border: none;
+      display: flex;
+      flex-direction: row;
+      height: 25vh;
+      justify-content: flex-end;
+      overflow-x: auto;
+      width: 100%;
+    }
+`;
+  
+export const NavItem = styled.li`
+    align-items: center;
+    border-bottom: 1px solid ${cinzaMedio};
+    display: flex;
+    flex-direction: row;
+    height: 10vh;
+    justify-content: flex-start;
+    text-decoration: none;
+    width: 100%;
+  
+    &:first-child {
+      border-top: 1px solid ${cinzaMedio};
+      margin-left: 0;
+      margin-top: 7.5%;
+      //-------------------------------------------->
+      @media screen and (max-width: 768px) {
+        border-bottom: 1px solid ${cinzaMedio}; //pode retirar se quiser
+        border-top: none;
+        flex-direction: row-reverse;
+        justify-content: space-between;
+        margin-top: 0;
+        position: absolute;
+        left: 0;
+        top: 5%;
+        width: 100vw;
+      }
+    }
+  
+    &:hover {
+      border-left: 4px solid ${azulEscuroFosco};
+      cursor: pointer;
+      //adicionar o material icons aqui
+      //.material-icons {
+      /* color: $azul-medio-brilhante;
+                  }
+                  a {
+                      color: black;
+                  } */
+  
+      //-------------------------------------------->
+      @media screen and (max-width: 768px) {
+        border-left: none;
+      }
+    }
+    //-------------------------------------------->
+    @media screen and (max-width: 768px) {
+      align-items: center;
+      border-bottom: none;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      margin: 0 5%;
+      width: 100%;
+    }
+`;
+
+export const NavIcon = styled.i`
+    color: ${cinzaEscuro};
+    cursor: pointer;
+    font-size: 0.8rem;
+    padding: 0 7.5%;
+  
+    //---------------------------------------------------->
+    @media screen and (max-width: 768px) {
+      padding: 0;
+    }
+`;
+  
+export const NavLink = styled.p`
+    color: ${cinzaEscuro};
+    font-size: 1rem;
+    font-family: "Lato", sans-serif;
+    text-decoration: none;
+    width: 100%;
+    //-------------------------------------------->
+    @media screen and (max-width: 768px) {
+      width: 75%;
+      padding-left: 10%;
+    }
+`;
