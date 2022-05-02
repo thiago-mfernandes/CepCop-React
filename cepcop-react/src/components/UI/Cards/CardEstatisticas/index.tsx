edb03de1ae19e3b088797ab2bfcdca905084a255
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-05-02 14:45:27 -0300

    Card Estatisticas, Titulo implementados

---
+++ cepcop-react/src/components/UI/Cards/CardEstatisticas/index.tsx
@@ -0,0 +1,12 @@
+import { StyledEstatisticas } from "./styled";
+import Titulo from "./Titulo";
+
+function Estatisticas() {
+    return (
+        <StyledEstatisticas>
+            <Titulo titulo="Cadastros este mês e no mês passado:" />
+        </StyledEstatisticas>
+    )
+}
+
+export default Estatisticas
---
+++ cepcop-react/src/components/UI/Cards/CardEstatisticas/styled.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { branco, brancoBg, cinzaEscuro } from "../../variaveis";
+
+export const StyledEstatisticas = styled.section`
+    background-color: ${branco};
+    border-radius: 24px;
+    box-shadow: 0px 4px 50px 0px rgba(0, 0, 0, 0.15);
+    grid-column: 1 / 5;
+    width: 100%;
+
+    //----------------------------------------------------->
+    @media screen and (max-width: 768px) {
+        grid-row: 6 / 9;
+        box-shadow: 0 4px 50px 0 rgba(0, 0, 0, 0.15);
+    }
+`;
+
+export const StyledTitulo = styled.p`
+    color: ${brancoBg};
+    font-size: 1.25rem;
+    background-color: ${cinzaEscuro};
+    border-radius: 24px 24px 0 0;
+    height: 10vh;
+    padding: 2.5%;
+    margin-bottom: 1.5%;
+    text-align: center;
+
+    //-------------------------------------------------->
+    @media screen and (max-width: 768px) {
+        height: 7.5vh;
+    }
+`;
+
+//@media screen and (max-width: 768px)
